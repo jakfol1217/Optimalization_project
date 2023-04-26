28892fa970102e2acbc608a628f5814050707452
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2023-04-26 19:32:42 +0200

    more info

--- Algorithm.py
+++ Algorithm.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 
-def loss_function(x, y):
+def loss_function(x, y, z):
+    # x -- parameter vector, in literature usually w
+    # y -- feature vector, y is in R^n
+    # z -- response vector, with values {-1,1}
+    # https://www.csie.ntu.edu.tw/~cjlin/papers/cdl2.pdf <- can't we use one of those methods?
     sm = 1 - x.T @ y
     return max(0, sm)
 
