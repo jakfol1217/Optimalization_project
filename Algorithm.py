454f601c825d04a4e2a7a8ce1f2cc01a80bbb1c0
jakfol1217 <56119515+jakfol1217@users.noreply.github.com>
2023-05-17 21:56:29 +0200

    add stopping condition and testing

--- Algorithm.py
+++ Algorithm.py
@@ -5,36 +5,53 @@ import numpy as np
 import copy
 from SVM import loss_function
 
-
-class coordinate_descend:
-    def __init__(self, C, beta=0.5, ro=0.01):
+# Algorithm is from
+# https://www.csie.ntu.edu.tw/~cjlin/papers/cdl2.pdf
+class CoordinateDescent:
+    def __init__(self, C, beta=0.5, ro=0.01, eps=1e-15, max_iter=500):
         # C - regularization parameter
         # beta, ro - algoritm parameters
         self.C = C
-        self.beta = beta
-        self.ro = ro
+        self.beta = beta # in (0, 1)
+        self.ro = ro # in (0, 1/2)
+        self.eps = eps # solution accuracy
+        self.max_iter = max_iter
         self.x = None
         self.y = None
         self.H = None
+        self.D = np.array([])
+
 
     def fit(self, x, y):
         # x - data
         # y - responses
-
-        # weight initialization
+        # to add bias just include a column of ones
         self.x = x
         self.y = y
-        w = np.random.normal(size=x.shape[1])
         Hs = [self._H(i) for i in range(x.shape[1])]
         self.H = Hs
-        # initialize all the H_i, permutation of w, stop condition
+
+    def process(self):
+        if self.x is None or self.y is None or self.H is None:
+            raise Exception("Algorithm is not fitted yet")
+        # weight initialization
+        w = np.zeros(self.x.shape[1])
         stop_condition = True
-        while not stop_condition:
+        # stop condition- max iteration or max(D'_i(0)) small enough or sum(D'_i(0)^2) small enough or validation error
+        iter = 0
+        stop = self.eps + 1
+        while iter < self.max_iter and stop >= self.eps:
+            self.D = np.array([])
             idx = np.random.permutation(len(w))
             for i in idx:
                 z = self._sub_problem(w, i)
                 w[i] += z
-        # TODO - find stop condition
+            iter += 1
+            stop = sum(self.D**2)
+        self.clear()
+        return w
+
+    def clear(self):
         self.H = None
         self.x = None
         self.y = None
@@ -42,7 +59,9 @@ class coordinate_descend:
 
     def _sub_problem(self, w, i):
         D_hat_hat = self._D_hat_hat(w, 0, i)
-        d = -self._D_hat(w, 0, i)/D_hat_hat
+        D_hat = self._D_hat(w, 0, i)
+        self.D = np.append(self.D, D_hat)
+        d = -D_hat/D_hat_hat
         lam = 1
         z = lam * d
         while True: #:(
@@ -54,7 +73,6 @@ class coordinate_descend:
             z *= self.beta
         return z
 
-        # TODO - testing
 
     def _b(self, w, j):
         bj = 1 - self.y[j] * w.T @ self.x[j,:]
@@ -67,8 +85,8 @@ class coordinate_descend:
         sm = 0
         for j in range(len(self.y)):
             if self._indi_b(wz, j):
-                sm += _b(wz, j)**2
-        res += C * sm
+                sm += self._b(wz, j)**2
+        res += self.C * sm
         return res
 
 
@@ -79,8 +97,8 @@ class coordinate_descend:
         sm = 0
         for j in range(len(self.y)):
             if self._indi_b(wz, j):
-                sm += y[j] * x[j,i] * _b(wz, j)
-        res -= 2 * C * sm
+                sm += self.y[j] * self.x[j,i] * self._b(wz, j)
+        res -= 2 * self.C * sm
         return res
 
     def _D_hat_hat(self, w, z, i):
@@ -90,11 +108,12 @@ class coordinate_descend:
         sm = 0
         for j in range(len(self.y)):
             if self._indi_b(wz, j):
-                sm += x[j, i]**2
-        res += 2 * C * sm
+                sm += self.x[j, i]**2
+        res += 2 * self.C * sm
+        return res
 
     def _indi_b(self, w, j):
-        return self.b(w, j) > 0
+        return self._b(w, j) > 0
 
     def _H(self, i):
         if self.H is not None:
